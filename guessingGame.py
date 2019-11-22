7748ec6cddda141097381452c208d9dbfcd460ac
Douglas <369881@mcpsmd.org>
2019-11-22 11:49:20 -0500

    Prints out if the number is right or not

--- guessingGame.py
+++ guessingGame.py
@@ -1,4 +1,9 @@
 import random
-guess = random.randint(1,100)
-answer = "s"
-
+value = random.randint(1,101)
+guess = int(input("I am thinking of a number between one through one hundred. Enter your guess: "))
+if guess > value:
+    print("guess is too high")
+elif guess < value:
+    print("value is too low")
+else :
+    print("You got it!")
