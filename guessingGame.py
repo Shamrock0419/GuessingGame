59e6b82516553290e6b1d2673fff70a91c30065e
Douglas <369881@mcpsmd.org>
2019-11-26 09:22:24 -0500

    finished guessing game

--- guessingGame.py
+++ guessingGame.py
@@ -1,9 +1,23 @@
 import random
+
+guessesTaken = 0
+
 value = random.randint(1,101)
-guess = int(input("I am thinking of a number between one through one hundred. Enter your guess: "))
-if guess > value:
-    print("guess is too high")
-elif guess < value:
-    print("value is too low")
-else :
-    print("You got it!")
+while guessesTaken < 10:
+    guess = int(input("I am thinking of a number between one through one hundred. Enter your guess: "))
+    
+    guessesTaken = guessesTaken + 1
+
+    if guess > value:
+        print("guess is too high, try again")
+
+    elif guess < value:
+        print("guess is too low, try again")
+
+    else :
+        print("You got it! You guessed the number in " + str(guessesTaken) + " attempts!")
+        break 
+
+if guessesTaken == 10:
+    print("Sorry, the number I was thinking of was " + str(value))
+
